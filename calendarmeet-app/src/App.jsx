048c1ad18a075388366dc2a3aa545a05d064e6cf
erikignacio <98852108+erikignacio@users.noreply.github.com>
2025-12-01 22:31:02 -0800

    added google analytics tag

--- calendarmeet-app/src/App.jsx
+++ calendarmeet-app/src/App.jsx
@@ -2,6 +2,15 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import EventsSection from "./components/EventsSection";
+import ReactGA from "react-ga4";
+
+const MEASUREMENT_ID = "G-3JKPKPVV9C";
+ReactGA.initialize(MEASUREMENT_ID);
+
+useEffect(() => {
+        ReactGA.initialize('G-YOUR_ID_HERE');
+        ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+      }, []);
 
 function App() {
   return (
